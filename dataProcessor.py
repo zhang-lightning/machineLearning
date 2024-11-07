5c2056a290f23c75729b7f8ec30dd3fe567ca007
荆川 <zeshen321@outlook.com>
2024-11-07 11:27:27 +0800

    remove .gitignore

--- dataProcessor.py
+++ dataProcessor.py
@@ -7,6 +7,15 @@ save as a xlsx file.
 """
 import os
 import numpy as np
+import pandas as pd
 import datetime as dt
 
-files = os.listdir()
+files = os.listdir("./data/")
+names = ["time", "lap1"]
+for file in files:
+    if file[-3:]=='csv':
+        path = "./data/" + file
+        data = pd.read_csv(path, sep=",")
+        
+    else:
+        continue
